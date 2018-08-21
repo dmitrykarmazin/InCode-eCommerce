38ed1275ff9e809056c74eeb046dc136f0519fe8
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2018-08-21 16:13:17 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,19 @@ import { HomeModule } from './modules/home/home.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+import { CustomMaterialModule } from './modules/custom-material/custom-material.module';
+
+import { StoreModule } from '@ngrx/store';
+import { productReducer } from './core/store/products/product.reducer';
+
+
+import { EffectsModule } from '@ngrx/effects';
+import { ProductEffects } from './core/store/products/product.effect';
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -13,7 +24,17 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HomeModule
+    CustomMaterialModule,
+    HttpClientModule,
+    // material
+
+    //
+    HomeModule,
+    StoreModule.forRoot({'product': productReducer} ),
+    EffectsModule.forRoot([ProductEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
