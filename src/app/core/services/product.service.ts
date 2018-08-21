b3add8452a13911a28e0404bcb2bcfc1ff5565bc
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2018-08-21 16:05:53 +0300

    add product service + gen json file

---
+++ src/app/core/services/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Product } from '../models/product';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable()
+export class ProductService {
+  private url = '/assets/products.json';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.url);
+  }
+}
