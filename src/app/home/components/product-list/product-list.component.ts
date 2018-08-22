2f6b9b4bcc200fe74cf5c56783c181026cbcff25
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2018-08-22 17:05:15 +0300

    add ObservableMedia for responsive grid-list

--- src/app/home/components/product-list/product-list.component.ts
+++ src/app/home/components/product-list/product-list.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy} from '@angular/core';
+import { Component, AfterContentInit,  ViewChild, OnInit, Input, ChangeDetectionStrategy} from '@angular/core';
 import { Product } from '../../models/product';
+
+import { MediaChange, ObservableMedia } from '@angular/flex-layout';
+import { MatGridList } from '@angular/material';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-product',
@@ -7,12 +13,26 @@ import { Product } from '../../models/product';
   styleUrls: ['./product-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
-
-  constructor() { }
+export class ProductListComponent implements OnInit, AfterContentInit {
 
   @Input() products: Product[];
 
-  ngOnInit() {
+  gridByBreakpoint = {
+    xl: 8,
+    lg: 6,
+    md: 4,
+    sm: 2,
+    xs: 1
+  };
+  cols$: Observable<number>;
+  constructor(private observableMedia: ObservableMedia) { }
+
+  ngOnInit() { }
+
+  ngAfterContentInit() {
+    this.cols$ = this.observableMedia.asObservable()
+    .pipe(
+      map(change => this.gridByBreakpoint[change.mqAlias])
+    );
   }
 }
