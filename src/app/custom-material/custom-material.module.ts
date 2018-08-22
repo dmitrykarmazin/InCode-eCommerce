ddc25e4e471a12932b11004c4c0b2a2ae2498fa6
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2018-08-22 17:00:44 +0300

    updates(imports,styles,tempates)

--- src/app/custom-material/custom-material.module.ts
+++ src/app/custom-material/custom-material.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
     MatGridListModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
-  exports: [ CommonModule, MatCardModule , MatGridListModule , MatButtonModule]
+  exports: [ CommonModule, MatCardModule , MatGridListModule , MatButtonModule, MatToolbarModule, FlexLayoutModule]
 })
 export class CustomMaterialModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
-    imports: []
+    imports: [],
+    declarations: [HeaderComponent, FooterComponent]
 
 })
 export class SharedModule {}
